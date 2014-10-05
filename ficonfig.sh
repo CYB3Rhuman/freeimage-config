6b51865316de010c55cafb2502a80664155551cc
Dmitry Golovin <dima@golovin.in>
2014-10-05 14:21:15 +0300

    fix condition again

--- ficonfig.sh
+++ ficonfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FI_VERSION=3.15.4
+FI_VERSION=3.16.0
 FI_ARCHIVE="FreeImage${FI_VERSION//./}.zip"
 
 function fi_get {
@@ -26,7 +26,7 @@ function fi_build {
 
 function fi_analyze {
     if [ -d "FreeImage" ]; then
-        if [ -e "FreeImage/Makefile.srcs" ] || [ -e "FreeImage/Source/Plugin.h" ]; then
+        if [ -e "FreeImage/Makefile.srcs" ] && [ -e "FreeImage/Source/Plugin.h" ]; then
             FI_LIBRARIES=$(grep INCLUDE "FreeImage/Makefile.srcs" | sed -e 's/^.*-ISource //g' \
                 -e 's/-ISource\///g' -e 's/\/[A-z]*//g' -e 's/Metadata \|FreeImageToolkit //g' | \
                 awk 'BEGIN{RS=ORS=" "}!a[$0]++' | tr -d '\n')
